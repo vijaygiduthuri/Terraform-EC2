d7038e8c28c747b1eae11e6c8fae79a359b987c3
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2024-02-07 21:43:14 +0530

    Main.tf

--- Main.tf
+++ Main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "Jenkins-sg" {
 resource "aws_instance" "web" {
   ami                    = "ami-03f4878755434977f"
   instance_type          = "t2.medium"
-  key_name               = "Mumbai"
+  key_name               = "mumbai"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
 
